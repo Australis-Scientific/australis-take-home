e0ef43a2412377c4bca41a5dd94f6ecd80b0fa20
allanevans170 <allanevans170@gmail.com>
2025-06-11 10:30:21 -0600

    add search bar and display results from pokemon search

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import SearchBar from "./components/searchBar";
 
 function App() {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Welcome to the Challenge!</h1>
-      <p>Start building here.</p>
+      <h1>pokeSearch</h1>
+      <SearchBar />
     </div>
   );
 }
